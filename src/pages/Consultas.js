a9de33d3c2ef5e3a74058b89b49216a7f6d8adc4
HenriqueSantosHS <henriquelp321@gmail.com>
2023-11-13 20:33:24 -0300

    implementando tela de consultas #6

---
+++ src/pages/Consultas.js
@@ -0,0 +1,56 @@
+import * as React from 'react';
+import { SafeAreaView, Text, FlatList } from 'react-native';
+import { Button } from 'react-native-paper';
+import Header from '../components/Header';
+
+export default function Consultas({ props, navigation }) {
+
+  const TextStyle = {
+    fontSize: 32,
+    textAlign: 'center',
+    fontWeight: '400',
+    marginHorizontal: 24,
+    paddingTop: 16,
+  };
+
+  const botaoStyle = {
+    backgroundColor: '#007AFF',
+    borderRadius: 24,
+    position: 'absolute',
+    bottom: 16,
+  };
+
+  const data = [
+    {
+      id: '1',
+      tipoConsulta: 'Ortopedia',
+      horario: '10h15',
+      local: 'Hospital Ortopédico',
+    },
+    {
+      id: '2',
+      tipoConsulta: 'Cardiologia',
+      horario: '17h00',
+      local: 'Cardio+',
+    },
+  ];
+
+  return (
+    <>
+      <Header title="Consultas" />
+      <Text style={TextStyle}>Suas Consultas</Text>
+      <SafeAreaView style={{ flex: 1, alignItems: 'center', marginTop: 24 }}>
+        <FlatList
+          data={data}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <ExibirConsultas dado1={item.tipoConsulta} dado2={item.horario} dado3={item.local} />
+          )}
+        />
+        <Button style={botaoStyle} onPress={() => navigation.navigate('')}>
+          <Text style={{ color: 'white', fontSize: 24, paddingTop: 8 }}>+</Text>
+        </Button>
+      </SafeAreaView>
+    </>
+  );
+}
