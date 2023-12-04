661e87c56ee78736d2e7e8ea9b4be623a810ed99
Kievv <ievsouza622@gmail.com>
2023-12-04 14:11:02 -0300

    Corrigindo bug de exibição de itens da Flatlist da página de Exames resolves #78

--- src/components/ExibirExames.js
+++ src/components/ExibirExames.js
@@ -5,6 +5,7 @@ import {
   Modal,
   TouchableWithoutFeedback,
   View,
+  StyleSheet,
 } from "react-native";
 import { Checkbox } from "react-native-paper";
 import { Button } from "react-native-paper";
@@ -16,31 +17,6 @@ export default function ExibirExames(props) {
   const [isChecked, setIsChecked] = useState(props.compareceu ? true : false);
   const [isVisible, setIsVisible] = useState(false);
   const { removerExame } = useContext(ExamesContext);
-
-  const exibirStyle = {
-    flex: 1,
-    flexDirection: "row",
-    alignItems: "center",
-    borderWidth: 1,
-    borderColor: "#007AFF",
-    borderRadius: 10,
-    maxHeight: 120,
-    maxWidth: 300,
-    width: 300,
-    paddingHorizontal: 16,
-    marginTop: 16,
-  };
-
-  const TextStyle = {
-    marginHorizontal: 0,
-    paddingHorizontal: 16,
-    paddingVertical: 8,
-  };
-
-  const checkboxStyle = {
-    marginHorizontal: 16,
-    marginLeft: 16,
-  };
 
   const showModal = () => {
     setIsVisible(true);
@@ -61,19 +37,19 @@ export default function ExibirExames(props) {
     hideModal();
   };
   return (
-    <SafeAreaView style={exibirStyle}>
+    <View style={styles.exibirStyle}>
       <Checkbox
-        style={checkboxStyle}
+        style={styles.checkboxStyle}
         label=""
         status={isChecked ? "checked" : "unchecked"}
         onPress={handleCheck}
       />
       <View style={{ flexDirection: "column" }}>
-        <Text style={TextStyle}>{props.exame}</Text>
-        <Text style={TextStyle}>{props.local}</Text>
+        <Text style={styles.TextStyle}>{props.exame}</Text>
+        <Text style={styles.TextStyle}>{props.local}</Text>
         <View style={{ flexDirection: "row" }}>
-          <Text style={TextStyle}>{props.data}</Text>
-          <Text style={TextStyle}>{props.horario}</Text>
+          <Text style={styles.TextStyle}>{props.data}</Text>
+          <Text style={styles.TextStyle}>{props.horario}</Text>
           <TouchableWithoutFeedback onPress={showModal}>
             <View>
               <FontAwesomeIcon
@@ -123,6 +99,33 @@ export default function ExibirExames(props) {
           </View>
         </View>
       </Modal>
-    </SafeAreaView>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  exibirStyle: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    borderWidth: 1,
+    borderColor: "#007AFF",
+    borderRadius: 10,
+    maxHeight: 120,
+    maxWidth: 300,
+    width: 300,
+    paddingHorizontal: 16,
+    marginTop: 16,
+  },
+
+  TextStyle: {
+    marginHorizontal: 0,
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+  },
+
+  checkboxStyle: {
+    marginHorizontal: 16,
+    marginLeft: 16,
+  },
+});
